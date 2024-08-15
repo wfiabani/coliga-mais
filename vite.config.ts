b0f40489f3e5aa9a3355747c315e752b53c7c327
Wilian Fiabani <fiabani.wilian@gmail.com>
2024-08-15 15:46:49 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://wfiabani.github.io/mdb/', 
+  base: 'https://wfiabani.github.io/mdb/dist/', 
 })
